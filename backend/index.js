d1401f8c021a826dfce70fb3e62b80e5f19b815d
RichardrLin <richardr.lin@mail.utoronto.ca>
2025-11-23 23:38:21 -0500

    T11

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import routes from "./routes.js";
+import cors from "cors";
+import dotenv from "dotenv";
+// TODO: complete me (loading the necessary packages)
+
+dotenv.config();
+
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
+const app = express();
+
+app.use(cors({
+  origin: FRONTEND_URL,
+  credentials: true,
+}));
+
+// TODO: complete me (CORS)
+app.use(express.json());
+app.use('', routes);
+
+export default app;
